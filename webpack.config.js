2bcd32e974b553c9b73f86e73f40d09e477d8df3
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-09-01 09:22:58 -0700

    change file names

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     plugins: [new ESLintPlugin(options)]// new line
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'RPG',
       template: './src/index.html',
       inject: 'body'
     })
