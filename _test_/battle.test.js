b2145e81134f2427a6f7afc7368a93a3c7029be7
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-09-01 16:52:48 -0700

    jest basic combat working as intended

--- _test_/battle.test.js
+++ _test_/battle.test.js
@@ -72,7 +72,7 @@ describe ('kungFu', () => {
     expect(boss.myStats[2] === 39);
   });
 
-  test('should return boss.myStats between 19 and 38', () => {
+  test('should return boss1.myStats between 19 and 38', () => {
     let myChar1 = new Character("Dragon");
     myChar1.checkType(myChar.myType);
     let boss1 = new Character("Turtle");
@@ -85,6 +85,29 @@ describe ('kungFu', () => {
     charDamage1 -= defenseMod;
     boss1.myStats[2] = (boss1.myStats[2] - charDamage1);
     expect( 18 < boss1.myStats[2] < 39);
+  });
+
+  test('should return char1.myStats and boss1.myStats modified by respective damage', () => {
+    let myChar1 = new Character("Dragon");
+    myChar1.checkType(myChar.myType);
+    let boss1 = new Character("Turtle");
+    boss1.checkType(boss.myType);
+    let charDamage1 = 0;
+    let bossDamage1 = 0;
+    let charAtk = Math.floor(myChar1.myStats[0] * .10);
+    let bossAtk = Math.floor(boss1.myStats[0] * .10);
+    let bossDef = Math.floor(boss1.myStats[1] * .10);
+    let charDef = Math.floor(myChar1.myStats[1] * .10);
+    charDamage1 += rollDice(1,20);
+    charDamage1 += charAtk;
+    charDamage1 -= bossDef;
+    boss1.myStats[2] = (boss1.myStats[2] - charDamage1);
+    bossDamage1 += rollDice(1,20);
+    bossDamage1 += bossAtk;
+    bossDamage1 -= charDef;
+    myChar1.myStats[2] = (myChar1.myStats[2] - bossDamage1);
+    console.log(boss1.myStats[2])
+    console.log(myChar1.myStats[2])
   });
 })
 
